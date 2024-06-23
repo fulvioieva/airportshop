5620e13ddac1f526c169fa378dbbd8c0c8d07f8c
TriscariGiorgio <giorgiotriscari@gmail.com>
2024-06-23 16:17:10 +0200

    Update FlightService.java

--- src/main/java/com/example/demo/service/FlightService.java
+++ src/main/java/com/example/demo/service/FlightService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +17,9 @@ public class FlightService implements IFlightService {
 	
 	@Autowired
 	FlightRepository flightRepository;
-
-	String[] valoriNonAmmessi = {".","-","_"};
 	
+	String[] valoriNonAmmessi = {".","-","_"};
+
 	@Override
 	public boolean addFlight(Flight flight) {
 		List<String> listName = new ArrayList<>();
@@ -41,7 +41,7 @@ public class FlightService implements IFlightService {
 
 	@Override
 	public boolean updateFlight(Flight flight) {
-		if (flightRepository.findById(flight.getId_flight()).isEmpty()) {
+		if (flightRepository.findById(flight.getId()).isEmpty()) {
 			return false;
 		}
 		for (String x : flight.getName().trim().split("")) {
@@ -50,7 +50,7 @@ public class FlightService implements IFlightService {
 			}
 		}
 		flightRepository.save(flight);
-		return true;
+		return false;
 	}
 
 	@Override
@@ -78,34 +78,5 @@ public class FlightService implements IFlightService {
 		return true;
 	}
 
-	@Override
-	public List<Flight> getFlightsOrigin(String city) {
-		List<Flight> listFlight = new ArrayList<Flight>();
-		listFlight = flightRepository.findByOriginLike(city);
-		if (listFlight.isEmpty()) {
-			throw new NullPointerException();
-		}
-		return listFlight;
-	}
-
-	@Override
-	public List<Flight> getFlightBetween(Date date, Date date2) {
-		List<Flight> listFlight = new ArrayList<Flight>();
-		listFlight = flightRepository.findByDateBetween(date,date2);
-		if (listFlight.isEmpty()) {
-			throw new NullPointerException();
-		}
-		return listFlight;
-	}
-
-	@Override
-	public List<Flight> getFlightsDestination(String city) {
-		List<Flight> listFlight = new ArrayList<Flight>();
-		listFlight = flightRepository.findByDestinationLike(city);
-		if (listFlight.isEmpty()) {
-			throw new NullPointerException();
-		}
-		return listFlight;
-	}
-
+	
 }
