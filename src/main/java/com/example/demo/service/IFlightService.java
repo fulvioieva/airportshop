d0052929aa74b9554c67d8a69831ce09c0f040b9
TriscariGiorgio <giorgiotriscari@gmail.com>
2024-06-23 16:17:06 +0200

    Update IFlightService.java

--- src/main/java/com/example/demo/service/IFlightService.java
+++ src/main/java/com/example/demo/service/IFlightService.java
@@ -1,6 +1,5 @@
 package com.example.demo.service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,7 +11,4 @@ public interface IFlightService {
 	List<Flight> getAllFlight();
 	Optional<Flight> getFlight(int idFlight);
 	boolean deleteFlight(int idFlight);
-	List<Flight> getFlightsOrigin(String city);
-	List<Flight> getFlightBetween(Date date, Date date2);
-	List<Flight> getFlightsDestination(String city);
 }
